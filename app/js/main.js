e621c802e8284bbfd27625d094f2a545bdda10fa
Simeon_Kolchin <62173656+hanter-ultra@users.noreply.github.com>
2020-07-21 23:09:00 +0300

    Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+
+  $('.products__slider').slick({
+    prevArrow: '<buttom class="slider-btn slider-btn__left"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.21839 1L1 9L9.21839 17"/></svg></buttom>',
+    nextArrow: '<buttom class="slider-btn slider-btn__right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.78161 17L9 9L0.78161 1"/></svg></buttom>',
+    infinite: false,
+  })
+
+
+
+    $('.questions__item-title').on('click', function () {
+      $('.questions__item').removeClass('questions__item--active');
+      $(this).parent().addClass('questions__item--active');
+    });
+  
+  $('#fullpage').fullpage({
+    autoScrolling: true,
+    scrollHorizontally: true,
+    sectionSelector: '.page-section',
+  });
+  
+});
